2c020c4b983a3e098565ac8b7ef9b480bef059f9
AnthonyKubeka <anthonykubeka@gmail.com>
2021-11-03 20:07:01 +0200

    Add properties to TagViewModel

--- FileNameTagger/FileNameTagger/Tag/TagViewModel.cs
+++ FileNameTagger/FileNameTagger/Tag/TagViewModel.cs
@@ -1,12 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain;
+using Shared;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace FileNameTagger.Tag
 {
-    public class TagViewModel
+    public class TagViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { }; //property change is never null since we assign it an empty anonymous subscriber
+        private ObservableCollection<Studio> studios;
+        private ObservableCollection<Actor> actors;
+        private ObservableCollection<ResolutionsEnum> resolutions;
+        private ObservableCollection<Category> categories;
+        private Studio selectedStudio;
+        private Actor selectedActor;
+        private Category selectedCategory;
+        private ResolutionsEnum selectedResolution;
+
+        public ObservableCollection<Studio> Studios
+        {
+            get
+            {
+                return studios;
+            }
+
+            set
+            {
+                if (studios != value)
+                {
+                    studios = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Studios"));
+                }
+            }
+        }
+
+        public ObservableCollection<Actor> Actors
+        {
+            get
+            {
+                return actors;
+            }
+
+            set
+            {
+                if (actors != value)
+                {
+                    actors = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Actors"));
+                }
+            }
+        }
+
+        public ObservableCollection<Category> Categories
+        {
+            get
+            {
+                return categories;
+            }
+
+            set
+            {
+                if (categories != value)
+                {
+                    categories = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Categories"));
+                }
+            }
+        }
+
+        public ObservableCollection<ResolutionsEnum> Resolutions
+        {
+            get
+            {
+                return resolutions;
+            }
+
+            set
+            {
+                if (resolutions != value)
+                {
+                    resolutions = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Resolutions"));
+                }
+            }
+        }
+
+        public TagViewModel()
+        {
+
+        }
     }
 }
