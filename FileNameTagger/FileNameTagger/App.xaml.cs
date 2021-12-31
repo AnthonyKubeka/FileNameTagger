782935694cc955bee93c17743aa5227e1f27b386
AnthonyKubeka <anthonykubeka@gmail.com>
2021-12-31 23:08:03 +0200

    finish concrete sqlite implementation (need to abstract later)

--- FileNameTagger/FileNameTagger/App.xaml.cs
+++ FileNameTagger/FileNameTagger/App.xaml.cs
@@ -12,6 +12,9 @@ namespace FileNameTagger
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
-    {
+    { //need to move to connection strings config of some kind
+        private static string databaseName = "FileNameTagger.db";
+        private static string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string databasePath = System.IO.Path.Combine(folderPath, databaseName);
     }
 }
--- FileNameTagger/FileNameTagger/MainWindow.xaml.cs
+++ FileNameTagger/FileNameTagger/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,7 @@ namespace FileNameTagger
         {
             InitializeComponent();
         }
+
+
     }
 }
