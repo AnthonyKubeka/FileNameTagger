92379613406a8b0b740d2cfe329d4d73b3c6e468
AnthonyKubeka <anthonykubeka@gmail.com>
2022-01-01 12:32:56 +0200

    Bin files

--- FileNameTagger/FileNameTagger/obj/Debug/net6.0-windows/MainWindow.g.cs
+++ FileNameTagger/FileNameTagger/obj/Debug/net6.0-windows/MainWindow.g.cs
@@ -78,7 +78,7 @@ namespace FileNameTagger {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FileNameTagger;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FileNameTagger;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
