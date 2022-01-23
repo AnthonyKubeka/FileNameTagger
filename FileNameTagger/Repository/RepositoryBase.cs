83cc43105e7a7e8e7389fc1fa2bc3337a11b969d
AnthonyKubeka <anthonykubeka@gmail.com>
2022-01-23 20:04:07 +0200

    add repository base, add CRUD methods for static data, tweak ui, fix scrolling for static data

---
+++ FileNameTagger/Repository/RepositoryBase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Repository
+{
+    public class RepositoryBase<T>: IRepositoryBase<T> where T:class, new()
+    {
+        private SQLiteAsyncConnection databaseConnection; 
+
+        public RepositoryBase(SQLiteAsyncConnection databaseConnection)
+        {
+            this.databaseConnection = databaseConnection; 
+        }
+
+        public async Task<List<T>> Get()
+        {
+            return await databaseConnection.Table<T>().ToListAsync();
+        }
+
+        public async Task<T> Get(int id)
+        {
+            return await databaseConnection.FindAsync<T>(id);
+        }
+
+        public async Task<List<T>> Get<TValue>(Expression<Func<T, bool>> predicate = null, Expression<Func<T, TValue>> orderBy = null)
+        {
+            var query = databaseConnection.Table<T>();
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (orderBy != null)
+            {
+                query = query.OrderBy(orderBy);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return await databaseConnection.FindAsync<T>(predicate);
+        }
+
+        public AsyncTableQuery<T> AsQueryable()
+        {
+            return databaseConnection.Table<T>();
+        }
+
+        public async Task<int> Create(T entity)
+        {
+            return await databaseConnection.InsertAsync(entity);
+        }
+
+        public async Task<int> Update(T entity)
+        {
+            return await databaseConnection.UpdateAsync(entity);
+        }
+
+        public async Task<int> Delete(T entity)
+        {
+            return await databaseConnection.DeleteAsync(entity);
+        }
+    }
+}
